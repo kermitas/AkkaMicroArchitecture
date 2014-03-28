7149f81cfc5b703e01fc4d3ed6eae824e0daf122
Arthur Stanek <artur@biodiscovery.com>
2014-03-28 16:12:01 +0100

    Changed reference to ama-core because other project has problem while downlading this project from GitHub.

--- project/Build.scala
+++ project/Build.scala
@@ -10,10 +10,10 @@ object Build extends Build {
   )
 
   lazy val root = Project(
-      id = "akkamicroarchitecture",
+      id = "akkamicroarchitecture", // name should be the same as folder to be 100% SBT like
       base = file("."),
       settings = projectSettings
     ).aggregate(ama_core).dependsOn(ama_core)
 
-  lazy val ama_core = RootProject(file("ama_core"))
+  lazy val ama_core = RootProject(file("./ama_core"))
 }
